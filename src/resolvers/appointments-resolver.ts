64d8d6670b3c387aadbf15da1fde006bc84d7ad9
Alexandre Kumagae <alexandre@movedigital.com.br>
2024-04-03 22:01:09 -0300

    feat: initial structure

---
+++ src/resolvers/appointments-resolver.ts
@@ -0,0 +1,16 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql"
+
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input"
+
+@Resolver()
+export class AppointmentsResolver {
+  @Query(() => String)
+  async helloWorld() {
+    return "Hello World!"
+  }
+
+  @Mutation(() => Boolean)
+  async createAppointment(@Arg('data', () => CreateAppointmentInput) data: CreateAppointmentInput) {
+    return true
+  }
+}
